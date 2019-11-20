e7820ef42f693d0a8a8acac789be7506b94ca4c6
Gustavo Frehse <gusfrehse@gmail.com>
2019-11-20 17:36:09 -0300

    Now I only need a way to add files containing blog posts

--- app.py
+++ app.py
@@ -6,35 +6,47 @@ app = Flask(__name__)
 
 posts = [
     {
+        'index':'0',
+        'title':'firs post!',
         'data':'this is a post cool massa tesão'
     },
     {
+        'index':'1',
+        'title':'second post',
         'data':'this is another post haha'
     }
 ]
 
 links = [
     {
-        'name':'google',
-        'href':'https://google.com'
+        'name':'start',
+        'href':'/'
     },
     {
         'name':'posts',
-        'href':'./posts'
+        'href':'/posts'
+    },
+    {
+        'name':'google',
+        'href':'https://google.com'
     }
 ]
 
 @app.route("/")
 @app.route("/index")
-def index():
+def show_index():
     title = "gsf's blog"
     return render_template('index.html', title=title,
-                           posts=posts, topLinks=links)
+                           posts=posts, top_links=links)
 
 @app.route("/posts")
-def posts():
+def show_posts():
     return render_template('posts.html', title='posts',
-                           posts=posts, topLinks=links)
+                           posts=posts, top_links=links)
+
+@app.route("/posts/<int:index>")
+def show_post(index):
+    return "this is post number %s" % index
 
 if __name__ == "__main__":
     app.run()
