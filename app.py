7586c2caa7d85e9cd7db2eb1b623b65f6c513427
Gustavo Frehse <gusfrehse@gmail.com>
2019-11-23 18:33:58 -0300

    Fixed order of display and other small details

--- app.py
+++ app.py
@@ -10,14 +10,21 @@ app = Flask(__name__)
 path = './posts'
 posts = []
 with os.scandir(path) as it:
-    post_index = 0
     for entry in it:
         if entry.is_file():
             with open(entry.path) as post:
-                posts.append({'index':post_index,
-                              'title':str(post_index),
-                              'data':post.read()})
+                data = post.read().split('\n', 2)
+                if len(data) != 3:
+                    print("[-] Bad formatting in post file %s" % entry.name)
+                print(data[0], data[1])
+                index = data[0]
+                title = data[1]
+                data = data[2]
+                posts.append({'index':int(index),
+                              'title':title,
+                              'data':data})
 
+posts = sorted(posts, key=(lambda d : d['index']))
 
 links = [
     {
@@ -37,19 +44,24 @@ links = [
 @app.route("/")
 @app.route("/index")
 def show_index():
-    title = "gsf's blog"
-    return render_template('index.html', title=title,
-                           posts=posts, top_links=links)
+    return render_template('index.html',
+                           title="gsf's blog",
+                           posts=posts[::-1][:4],
+                           top_links=links)
 
 @app.route("/posts")
 def show_posts():
-    return render_template('posts.html', title='posts',
-                           posts=posts, top_links=links)
+    return render_template('posts.html',
+                           title='posts',
+                           posts=posts,
+                           top_links=links)
 
 @app.route("/posts/<int:index>")
 def show_post(index):
-    return render_template('post.html', title='post',
-                           post=posts[index], top_links=links)
+    return render_template('post.html',
+                           title='post',
+                           post=posts[index],
+                           top_links=links)
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
